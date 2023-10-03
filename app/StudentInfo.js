adc979374484079bc769da60fdd3bf7c54ad2331
Walker-63 <buchananelijah06@gmail.com>
2023-10-02 21:06:02 -0600

    week2 assignment
    piuahing week 2 assignment

---
+++ app/StudentInfo.js
@@ -0,0 +1,21 @@
+export default function StudentInfo(){
+    return(
+        <main>
+            <div>
+
+                <p>
+                    Name: Elijah Buchanan
+                </p>
+
+                <p>
+                    Course Section: CPRG 306 E
+                </p>
+
+                <p>
+                    https://github.com/Walker-63/cprg306-assignments
+                </p>
+
+            </div>
+        </main>
+    );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,16 @@
+import StudentInfo from "/app/StudentInfo";
+
+export default function Page(){
+    return(
+        <main>
+            <div>
+                <h2>
+                    My Shopping List
+                </h2>
+
+                <StudentInfo />
+
+            </div>
+        </main>
+    );
+}
